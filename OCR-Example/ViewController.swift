27b80bfc54635bfd7f3647431f829b38625d87eb
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-10-17 14:24:23 +0200

    Initial Commit

---
+++ OCR-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR-Example
+//
+//  Created by Jan Niklas Steeg on 17.10.18.
+//  Copyright © 2018 Jan Steeg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
